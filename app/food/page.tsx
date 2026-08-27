e223f74cda8a15277bd314a6a270a6cb5c9bfc6b
gabriel lennon <gabriellennon7@gmail.com>
2026-08-27 16:10:56 +0100

    refactor: adjust home page to food

---
+++ app/food/page.tsx
@@ -0,0 +1,25 @@
+import { FoodCarousel } from "@/app/components/FoodCarousel";
+import { EventsGallerySection } from "@/app/components/EventsGallerySection";
+import { FooterSection } from "@/app/components/FooterSection";
+import { HeroSection } from "@/app/components/HeroSection";
+import { ContactCtaSection } from "@/app/components/ContactCtaSection";
+import { ContactSection } from "@/app/components/ContactSection";
+import { ServicesEventsSection } from "@/app/components/ServicesEventsSection";
+import { StorySection } from "@/app/components/StorySection";
+import { TestimonialsSection } from "@/app/components/TestimonialsSection";
+
+export default function FoodPage() {
+  return (
+    <>
+      <HeroSection />
+      <FoodCarousel />
+      <ServicesEventsSection />
+      <StorySection />
+      <EventsGallerySection />
+      <ContactCtaSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <FooterSection />
+    </>
+  );
+}
